dafe5ac3ad1fb99a0966c67f6e39d07393108f99
Soham Patel <sohampatel1483@gmail.com>
2026-03-28 17:26:51 +0530

    updated github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sohampatel.in",
+  output: "static",
   vite: {
     plugins: [tailwindcss()],
   },
